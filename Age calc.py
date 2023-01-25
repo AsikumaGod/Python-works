dedbe278dd40d7f9b0829533a93a9323240bb3ce
AsikumaGod <jacobantoine506@gmail.com>
2023-01-24 17:57:17 -0800

    Create Age calc.py
    Source code of my age calculator

---
+++ Age calc.py
@@ -0,0 +1,11 @@
+print("Answer the following questions let's see how you've lived")
+name = input("Name: ")
+print("what is your age?",(name),"?")
+age = int(input("age: "))
+years = age
+months = age * 12
+days = age * 365
+minutes = age * 525948
+seconds = age * 31556926
+print(name, ", you have been alive for",years,"years,",months,"months,",days,"days,",minutes,"minutes and",seconds,"seconds")
+print("thanks for using my age calculator")
